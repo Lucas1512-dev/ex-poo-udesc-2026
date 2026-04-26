c58b1ae8f0ff150703d5ed0d6b294cc2b8356097
Lucas de Oliveira <lucas.oliveira1512@edu.udesc.br>
2026-04-26 11:33:49 -0300

    Exercício 1 - Classe Livro e respectivo teste

---
+++ src/exercicio1oo/classes/Livro.java
@@ -0,0 +1,9 @@
+package exercicio1oo.classes;
+
+class Livro {
+    String titulo;
+    String autor;
+    String genero;
+    boolean emprestado;
+
+}
---
+++ src/exercicio1oo/classes/TestaLivro.java
@@ -0,0 +1,27 @@
+package exercicio1oo.classes;
+
+import java.util.Scanner;
+
+public class TestaLivro {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        Livro novo = new Livro();
+
+        System.out.print("Título :  ");
+        novo.titulo = leitor.nextLine();
+
+        System.out.print("Autor :  ");
+        novo.autor = leitor.nextLine();
+
+        System.out.print("Gênero :  ");
+        novo.genero = leitor.nextLine();
+
+        System.out.print("Emprestado (Sim: digitar true / Não: digitar false  :");
+        novo.emprestado = leitor.nextBoolean();
+
+        System.out.println("Título.....: " + novo.titulo);
+        System.out.println("Autor......: " + novo.autor);
+        System.out.println("Gênero........: " + novo.genero);
+        System.out.println("Emprestado.: " + novo.emprestado);
+    }
+}
