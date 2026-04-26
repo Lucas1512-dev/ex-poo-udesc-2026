1bdb37174610837249986cb7d67d7c14c2802ea6
Lucas de Oliveira <lucas.oliveira1512@edu.udesc.br>
2026-04-26 10:16:10 -0300

    Exercicio 1 - Classe Circulo e respectivo teste (coer stes / correções Testa Retangulo)

---
+++ src/exercicio1oo/classes/TestaCirculo.java
@@ -0,0 +1,24 @@
+package exercicio1oo.classes;
+
+import java.util.Scanner;
+
+public class TestaCirculo {
+    public static void main(String[] args){
+        Scanner valor = new Scanner(System.in);
+        Circulo novo = new Circulo();
+
+        System.out.print("Raio:  ");
+        novo.raio = valor.nextInt();
+        System.out.println("Raio: " + novo.raio);
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- src/exercicio1oo/classes/TestaRetangulo.java
+++ src/exercicio1oo/classes/TestaRetangulo.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 
 public class TestaRetangulo {
     public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
         Retangulo ret =  new Retangulo();
 
-        ret.altura = 23;
-        ret.largura = 14;
+        System.out.print("Altura:  ");
+        ret.altura = leitor.nextInt();
 
-        System.out.println("Altura:" + ret.altura);
-        System.out.println("Largura:" + ret.largura);
+        System.out.print("Largura:  ");
+        ret.largura = leitor.nextInt();
 
+        System.out.print("Retângulo com as seguintes medidas de altura: " + ret.altura + " e largura: " + ret.largura);
     }
 }
